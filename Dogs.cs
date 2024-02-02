a629c3019188f58b694c679b1699cc9177fcff3f
karinEkenberg <zirkon6@hotmail.com>
2024-02-02 12:55:48 +0100

    Add project files.

---
+++ Dogs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticStructExercise
+{
+    public struct Dogs
+    {
+        public static string Breed = "Akita";
+        public static int Height = 65;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace StaticStructExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The dog breed and size is:");
+            string breed = Dogs.Breed;
+            int height = Dogs.Height;
+            Console.WriteLine($"The {breed} are around {height} cm tall.");
+        }
+    }
+}
